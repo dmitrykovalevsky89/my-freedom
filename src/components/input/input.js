cadc275491efeb5eb28e1712babd63274dbfc48d
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2020-01-02 10:47:36 +0300

    lesson-2: create Input Создан компанент Input. Работа с регулярным выражением

---
+++ src/components/input/input.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./input.css";
+
+class Input extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			value: '',
+			label: 'Введите номер',
+			clsName: 'normal'
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.validPhone = this.validPhone.bind(this);
+	}
+
+	validPhone(evt) {
+		const validTest = /^\+375\[25|29]?|[33|44]?\d{12}$/;
+		// const validTest = /^\+\/375\/25|29\/\d{7}$/;
+		return validTest.test(evt);
+	};
+
+	handleChange(evt) {
+		const validTest = /^\+\d*\.?\d*$/;
+
+		// console.log(validTest.test(evt.target.value));
+		// if only + and number
+		if (validTest.test(evt.target.value)) {
+			this.setState({
+				value: evt.target.value
+			});
+
+			console.log(this.validPhone(evt.target.value));
+
+		} else {
+			this.setState({
+				label: 'Вводить только в формате +375...'
+			});
+		}
+
+		// console.log(evt.target.value);
+	};
+
+	render() {
+		return (
+			<>
+				<label>{this.state.label}</label><br/>
+				<input
+					type="text"
+					placeholder="+375..."
+					maxLength="13"
+					className={this.state.clsName}
+					value={this.state.value}
+					onChange={this.handleChange}
+				/>
+			</>
+		);
+	}
+}
+
+export default Input;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import { Shop } from "./components/shop/shop";
+import Input from "./components/input/input";
 import * as serviceWorker from './serviceWorker';
 import classNames from 'classnames';
 
 
+// Shop
+// ReactDOM.render(<Shop />, document.getElementById('root'));
 
-ReactDOM.render(<Shop />, document.getElementById('root'));
+// Input
+ReactDOM.render(<Input />, document.getElementById('root'));
 
 serviceWorker.unregister();
