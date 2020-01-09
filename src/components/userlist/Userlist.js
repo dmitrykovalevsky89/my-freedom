0864ef8b4c3863529efdd92fefb32bace7e5a3b6
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2020-01-09 21:10:24 +0300

    lesson-3: delete user Реализация удаления

--- src/components/userlist/Userlist.js
+++ src/components/userlist/Userlist.js
@@ -6,25 +6,40 @@ function addUser(users, userToAdd) {
 }
 
 export class Userlist extends React.Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        name: "Name 1",
-        phone: "+375291234567"
-      },
-      {
-        id: 2,
-        name: "Name 2",
-        phone: "+375291234568"
-      },
-      {
-        id: 3,
-        name: "Name 3",
-        phone: "+375291234569"
-      }
-    ]
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [
+        {
+          id: 1,
+          name: "Name 1",
+          phone: "+375291234567"
+        },
+        {
+          id: 2,
+          name: "Name 2",
+          phone: "+375291234568"
+        },
+        {
+          id: 3,
+          name: "Name 3",
+          phone: "+375291234569"
+        }
+      ]
+    };
+
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  deleteItem(index) {
+    const newItems = [...this.state.users];
+    newItems.splice(index, 1);
+
+    this.setState({
+      users: newItems
+    });
+  }
 
   nextId = 4;
 
@@ -37,32 +52,36 @@ export class Userlist extends React.Component {
               <th>ID</th>
               <th>Name</th>
               <th>Phone</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.users.map(user => (
-              <tr>
+            {this.state.users.map((user, index) => (
+              <tr key={index}>
                 <td>{user.id}</td>
                 <td>{user.name}</td>
                 <td>{user.phone}</td>
+                <td>
+                  <button onClick={() => this.deleteItem(index)}>Delete</button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
         <AddListItem
-            onSave={ (userName, userPhone) => {
-              const user = {
-                id: this.nextId,
-                name: userName,
-                phone: userPhone
-              };
+          onSave={(userName, userPhone) => {
+            const user = {
+              id: this.nextId,
+              name: userName,
+              phone: userPhone
+            };
 
-              this.setState({
-                users: addUser(this.state.users, user)
-              });
+            this.setState({
+              users: addUser(this.state.users, user)
+            });
 
-              this.nextId++;
-            }}
+            this.nextId++;
+          }}
         />
       </>
     );
