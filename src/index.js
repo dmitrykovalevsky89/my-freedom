997bc5a930d1a157f12e970d5224d6fb3c29796b
dmitrykovalevsky89 <dmitryk@gophotoweb.ru>
2020-01-25 21:55:28 +0300

    todolist: first step Созданы utils. InboxList с добавление tasks

--- src/index.js
+++ src/index.js
@@ -8,9 +8,14 @@ import Navbar from "./components/navbar/navbar";
 import { ClientList } from "./components/clientlist/ClientList";
 import * as serviceWorker from "./serviceWorker";
 import classNames from "classnames";
+import { TasksContainer } from "./pages/tasks/TasksContainer";
+import { InboxList } from "./pages/inbox/InboxList";
+
+// Task List
+ReactDOM.render(<InboxList />, document.getElementById("root"));
 
 // ClientList
-ReactDOM.render(<ClientList />, document.getElementById("root"));
+// ReactDOM.render(<ClientList />, document.getElementById("root"));
 
 // Shop
 // ReactDOM.render(<Shop />, document.getElementById("root"));
